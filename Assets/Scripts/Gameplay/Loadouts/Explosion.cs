f31f6fd81514d1d9bc4d58452bfdde685f505878
Daniel Walls <walls.dk@gmail.com>
2023-10-01 22:09:12 -0700

    cleanup and debug explosions

--- Assets/Scripts/Gameplay/Loadouts/Explosion.cs
+++ Assets/Scripts/Gameplay/Loadouts/Explosion.cs
@@ -1,15 +1,19 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Application.Gameplay.Combat;
 using Core;
 using FishNet;
 using FishNet.Object;
 using poetools.Core.Abstraction;
 using UnityEngine;
+using UnityEngine.Assertions;
 
 namespace Gameplay
 {
     public class Explosion : GameplayMonoBehavior
     {
+        private const int BufferSize = 50;
+
         [SerializeField]
         private int damage = 25;
 
@@ -22,14 +26,38 @@ namespace Gameplay
         [SerializeField]
         private float radius = 3;
 
+        // todo: pool?
+        [SerializeField]
+        private float lifetime = 1f;
+
+        private Collider[] _hitBuffer = new Collider[BufferSize];
+        private float _elapsedTime;
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = new Color(1, 0, 0, 0.5f);
+            Gizmos.DrawSphere(transform.position, radius);
+        }
+
+        private void Update()
+        {
+            _elapsedTime += Time.deltaTime;
+
+            if (_elapsedTime > lifetime)
+                Destroy(gameObject);
+        }
+
         public void Play(NetworkObject owner)
         {
             var damagedObjects = new List<NetworkObject>();
-            Collider[] hits = Physics.OverlapSphere(transform.position, radius);
+            int hits = Physics.OverlapSphereNonAlloc(transform.position, radius, _hitBuffer);
+            Assert.IsTrue(hits <= BufferSize);
             var player = Lobby.FindPlayer(owner);
 
-            foreach (Collider hit in hits)
+            for (int i = 0; i < hits; i++)
             {
+                var hit = _hitBuffer[i];
+
                 if (hit.TryGetComponentWithRigidbody(out NetworkObject hitObject))
                 {
                     var hitPlayer = Lobby.FindPlayer(hitObject);
